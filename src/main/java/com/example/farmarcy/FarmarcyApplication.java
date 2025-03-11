7e3dea96392924f6732f63589422bd1a13f02ab6
DaeyongLee <dy.lee@tosky.co.kr>
2025-03-11 10:43:47 +0900

    first commit

---
+++ src/main/java/com/example/farmarcy/FarmarcyApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmarcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmarcyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmarcyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farmarcy/FarmarcyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmarcy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmarcyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
